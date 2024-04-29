7390bf99d31ed03d4435f8056107816bbc85eee2
5pyD3R <96535745+3rr0r-505@users.noreply.github.com>
2024-04-29 23:15:28 +0530

    Update run-kSpy.ps1

--- src/gl0bal/run-kSpy.ps1
+++ src/gl0bal/run-kSpy.ps1
@@ -1,9 +1,9 @@
-# Path to the Node.js script
+# Path to the python script
 $pythonScriptPath = "C:\Program Files (x86)\winx32\kSpy.py"
 
 # Check if the Node.js script exists
 if (Test-Path $pythonScriptPath) {
-    # Run the Node.js script
+    # Run the python script
     python $pythonScriptPath
 } else {
     Write-Host "kSpy.py script not found at $pythonScriptPath"
